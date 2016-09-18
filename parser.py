db4344934d500460ad0572d3e0ac5671edbdc8b6
michailx <michail.xirouchakis@gmail.com>
2016-09-18 11:06:21 +0200

    third commit

--- parser.py
+++ parser.py
@@ -1,9 +1,37 @@
 from datetime import datetime
+import re
+
+# Read input file
 
 fr = open('./input_file/PM.txt', 'r')
 input_string = fr.read()
 fr.close()
-# do something with variable text
+
+# Parse input file (variable input_string)
+input_string = input_string.replace('\r', '')  # Remove /r at the end of each line
+lines = input_string.split('\n')
+
+# Note to self --> print(repr(string)) prints the raw string including all special chars like \n
+for line in lines:
+    line = line.strip()  # Remove beginning and trailing white spaces on each line
+    if line == "":
+        # Empty line
+        continue
+    elif re.match('^..:..:..\.', line) is not None:
+        # Timestamp
+        line = line[:8]
+    elif re.match('^30 second offered rate [0123456789]* bps', line) is not None:
+        # Rate
+        line = line[23:]
+        line = line.split(' ')[0]  # Need to improve this
+    else:
+        # Disregard
+        continue
+    print(repr(line))
+
+
+
+# Create CVS file
 
 file_name = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
 fw = open('./output_file/'+file_name+'.csv', 'w')
