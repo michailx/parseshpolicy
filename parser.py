25673492c0dfdd42c0272d0745f0b8c76d341a99
michailx <michail.xirouchakis@gmail.com>
2016-09-18 16:40:25 +0200

    First working version of the parser. It parses successfully the whole input file and generates an output CSV file. Since it doesn't understand the nested policy-map (HQF), class-default will appear twice. Within the same row, the leftmost entry is the parent whereas the rightmost is the child.

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from sys import exit
 import re
 
 # Read input file
@@ -11,12 +12,11 @@ fr.close()
 input_string = input_string.replace('\r', '')  # Remove /r at the end of each line
 lines = input_string.split('\n')
 
-sample_class_maps = []
-sample_bps = []
-sample = {}
+table = []
+table_head = ['timestamp']
+table_row = []
 
 i = 0
-
 # Note to self --> print(repr(string)) prints the raw string including all special chars like \n
 for line in lines:
     line = line.strip()  # Remove beginning and trailing white spaces on each line
@@ -25,41 +25,47 @@ for line in lines:
         continue
     elif re.match('^..:..:..\.', line) is not None:
         # Timestamp
-        # Next sample
-        i += 1
+        line = line[:8]
+
+        table_row = []
+        table_row.append(line)
 
         # Save previous sample
-        for n in range(0, len(sample_class_maps)):
-            sample[sample_class_maps[n]] = sample_bps[n]
-        print (sample)
+        if i == 1:
+            table.append(table_head)
+            table.append(table_row)
+        elif i > 1:
+            table.append(table_row)
 
-        # New sample
-        sample_class_maps = []
-        sample_bps = []
-        sample = {}
-
-        sample['timestamp'] = line[:8]
-        line = line[:8]  # DELETE THIS
+        # Next sample
+        i += 1
     elif re.match('^30 second offered rate [0123456789]* bps', line) is not None:
         # Rate
         line = line[23:]
         line = line.split(' ')[0]  # Need to improve this
-        sample_bps.append(line)
+        table_row.append(line)
     elif line[:11] == 'Class-map: ':
-        line = line[11:]
-        line = line.split(' ')[0]  # Need to improve this
-        sample_class_maps.append(line)
+        # Save class names. I only need to do this on the first sample, as it will be the same for all future samples.
+        if i == 1:
+            line = line[11:]
+            line = line.split(' ')[0]  # Need to improve this
+            table_head.append(line)
+        elif i > 1:
+            # Disregard
+            continue
     else:
         # Disregard
         continue
     #print(repr(line))
 
 
-
-# Create CVS file
+# Create CSV file
 
 file_name = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
 fw = open('./output_file/'+file_name+'.csv', 'w')
-fw.write('timestamp, total_input_traffic_bps, class-default, b1, b2, b3, rt2, rt, nc\n')
-fw.write('08:30:04, 45000, 0000, 0000, 40000, 1000, 0000, 0000, 4000\n')
+for row in table:
+    csv = ','.join(row)+'\n'
+    fw.write(csv)
+    print csv
+    csv = ''
 fw.close()
